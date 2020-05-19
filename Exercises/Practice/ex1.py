2c60c77aaed0a7ac0acd4daa3ce79b4bc220e403
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-19 05:04:42 +0000

    created

---
+++ Exercises/Practice/ex1.py
@@ -0,0 +1,4 @@
+#Assign a value to x in the first line, and print out the variable value in the second line
+
+x = 1998
+print(x)
---
+++ Exercises/Practice/var-value-2.py
@@ -0,0 +1,7 @@
+#Assign some values to varibales x,y and z. Then inseert x,y,z inside a prin() function to print out the values of those variables.
+
+x = "Believe"   #you can use numbers
+y = "in"
+z = "Yourself"
+
+print(x, y, z)
