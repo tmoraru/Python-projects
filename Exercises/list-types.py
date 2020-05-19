5d6dc2e5347dd41c9fbef16185a858f208e8876d
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-19 05:24:08 +0000

    created

--- Exercises/list-types.py
+++ Exercises/list-types.py
@@ -1,2 +1,15 @@
 #Into List you can add multiple objs
 
+student_grades = [9.1, 8.8, 7.5]
+
+
+# 1 ex:
+list(range(1, 10))
+
+#Output [1,2,3 ... 9]
+
+#2 ex:
+list(range(1, 10, 2))
+
+#Output [1, 3, 5, 7, 9]
+
