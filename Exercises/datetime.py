e775f70dbd85fcdd6e2c7f2e2eaeeecf045b6631
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-19 04:52:09 +0000

    created

--- Exercises/datetime.py
+++
@@ -1,5 +0,0 @@
-#How to see the time right now?
-
-import datetime    #datetime - is a module
-datetime.datetime.now()    #datetime - module, datime - obj of that module, now() - function
-
---
+++ Exercises/dt-time.py
@@ -0,0 +1,10 @@
+#How to see the time right now?
+
+import datetime    #datetime - is a module
+#print("The data and time is:") #will print a text in sepparate line
+print("The data and time is", datetime.datetime.now()) #print text and data in the same line
+print(datetime.datetime.now())    #datetime - module, datime - obj of that module, now() - function
+                                  #it will print the time
+
+
+
