a1542e7dbb8ced3adfe66163d2c7dfb7f80eb743
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-19 04:58:53 +0000

    created

---
+++ Exercises/variables1.py
@@ -0,0 +1,12 @@
+#Variables are names that you create in Py to store a value
+
+import datetime
+mynow = datetime.datetime.now() #mynow - var
+print(mynow)        #get value of var mynow
+
+#you can store numbers, text
+
+mynumber = 10
+mytext = "Hello"
+
+print(mynumber, mytext)
