efe24a4e44cdb3b1d21ed7b49900f455bec68b48
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-19 05:29:33 +0000

    created

---
+++ Exercises/Practice/temperatures.py
@@ -0,0 +1,9 @@
+#Assign a list to variable temperatures. The list should contain three items, float, int, str.
+
+temperatures = [20.5, 10, 'hot']
+
+#Assign a list to var rainfall. The list should contain four items, a float, an intenger, a string and a list.
+#Yes, there's nothing wrong with the lists containing other lists. Lists can contain any type.
+
+rainfall = [20.5, 10, 'hot', ['Tanya']]
+
