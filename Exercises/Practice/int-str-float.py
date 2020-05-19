dcbf8cf393f0e22968df1368bae7aeb152dbc735
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-19 05:18:43 +0000

    created

---
+++ Exercises/Practice/int-str-float.py
@@ -0,0 +1,17 @@
+#Create 3 vars mood, strenght and rank and assign a string to mood, a float to strenght and an int to rank. 
+#The values you assign can be anything as long as they of the correct type.
+
+mood = "Tanya"
+strenght = 22.1
+rank = 1998
+
+
+#Assign numbers x, y,z. Calculate the sum of x,y,z. Print out the values of s.
+
+x = 2
+y = 5
+z = 7
+s = x + z + y
+
+print(s)
+
