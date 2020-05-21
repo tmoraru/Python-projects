543872588735e23cbbb835d6c5dd2bb378d290f6
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-21 05:09:05 +0000

    added

--- Exercises/list-types.py
+++ Exercises/list-types.py
@@ -13,3 +13,5 @@ list(range(1, 10, 2))
 
 #Output [1, 3, 5, 7, 9]
 
+#how to add new item to list from CLI?  > >> monday_temperatures.append(6)
+#how to remove new item to list from CLI?  > >> monday_temperatures.remove(6)
---
+++ Exercises/tuple1.py
@@ -0,0 +1,8 @@
+#Tuple are like a list but they have () not square brackets[]
+
+monday_temperatures = (1, 4, 5)
+print(monday_temperatures) 
+
+
+
+
