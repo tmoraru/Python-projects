980a63da267b52512d4a5197620af2b7d6508f79
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-21 04:57:00 +0000

    added

---
+++ Exercises/dictionary1.py
@@ -0,0 +1,8 @@
+monday_temperatures = [9.1, 8.8, 8.8]
+student_grades = {"Mary": 9.1, "Dany": 8.8, "Jony": 8.8} #dictionary
+
+mysum = sum(student_grades.values()) 
+length = len(student_grades)
+mean = mysum / length
+print(mean)
+
