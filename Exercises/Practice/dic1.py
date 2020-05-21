18afee0bdaf5c43b34765d7ca0a8d2878201a890
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-21 05:01:48 +0000

    added

---
+++ Exercises/Practice/dic1.py
@@ -0,0 +1,4 @@
+#Assign a dictionary to variable day_temperatures. The dictionary should contain three keys, 
+#'morning' 'noon' 'evening'. Each key should have a float as a value.
+
+day_temperatures = {'morning': 5.1. 'noon': 6.3, 'evening': 7.3}
