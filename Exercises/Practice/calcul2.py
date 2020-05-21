27e5f17a5a41829b253544647cac7c4f0ebee37b
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-21 04:42:16 +0000

    added

---
+++ Exercises/Practice/calcul2.py
@@ -0,0 +1,5 @@
+#find the proper function or method that count how many 10.0 items there are in student_grades and print out 
+#the output using the print() function
+
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+print(student_grades.count(10.0))
