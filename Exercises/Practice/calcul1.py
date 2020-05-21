f650bc4e5de1681dcb7c6592e97540c4d5b57d29
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-21 04:36:57 +0000

    created

---
+++ Exercises/Practice/calcul1.py
@@ -0,0 +1,9 @@
+#Complete the script so that it prints out the maximum value of student_grades. In other world, your duty is to find out the 
+#apropiate function or method that runs the maximum value of a list and store its output in max_value.
+
+#print min_value and max_value from the list
+student_grades = [8.2, 8.8, 8.9]
+max_value = max(student_grades)
+min_value = min(student_grades)
+print(max_value, min_value)
+
