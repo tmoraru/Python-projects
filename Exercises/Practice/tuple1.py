17a7add5d9a0fbf0ceeb327bac81bcda535b4b3b
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-21 05:16:35 +0000

    added

---
+++ Exercises/Practice/tuple1.py
@@ -0,0 +1,9 @@
+#Create a color_codes variable and assign a tuple to it. The tuple should contain three tuples as intems
+
+color_codes = ((1,2,3), (1,2,3), (1,2,3))
+
+
+#Assign a dictionary day_temperature. The dict should contain 3 keys: 'morning', 'noon', 'evening
+#each key should contain a tuple as value. each tuple containt three floats
+
+day_temperatures = {'morning': (1.2, 4.3, 4.5), 'noon': (3.5, 2.3, 5.3), 'evening': (3.4, 4.5, 6.6)}
