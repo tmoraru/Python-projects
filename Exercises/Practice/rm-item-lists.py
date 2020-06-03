c8824e2fcd22105bfc22a89e2171b472c85ce2e5
Taniusellu <tatianamoraru1998@gmail.com>
2020-06-03 01:57:30 +0000

    added

---
+++ Exercises/Practice/rm-item-lists.py
@@ -0,0 +1,5 @@
+#Remove more items at once from the list
+seconds = [1.2323442655, 1.4534345567, 1.023458894, 1.10001399445]
+seconds.remove(1.4534345567)
+seconds.remove(1.023458894)
+seconds.remove(1.10001399445)
