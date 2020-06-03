be7323b93061d4730dd65176095f6c2b4de6a5d3
Taniusellu <tatianamoraru1998@gmail.com>
2020-06-03 01:52:40 +0000

    added

---
+++ Exercises/Practice/list-append.py
@@ -0,0 +1,5 @@
+#Append a value pf current to the end of the list seconds. Use list.append() method to do that
+
+seconds = [1.2323442655, 1.4534345567, 1.023458894]
+current = 1.10001399445
+seconds.append(current)
