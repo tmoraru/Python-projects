d8e2e193602a641ccba0774fc15ad9368a9c4a2d
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-06-11 12:17:35 -0500

    Update count1.py

--- Exercises/count1.py
+++ Exercises/count1.py
@@ -4,5 +4,5 @@ student_grades = [9.1, 8.8, 8.2]
 mysum = sum(student_grades)
 lenght = len(student_grades)  #len - return the number of items in a container
 mean = mysum / lenght
-print(mean)    
+print(mean)     
  
