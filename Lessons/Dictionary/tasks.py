a3435ccdb1622f8702b39e5a2b912c0beaf703a8
Taniusellu <tatianamoraru1998@gmail.com>
2020-07-08 04:44:20 +0000

    dictionary code

---
+++ Lessons/Dictionary/tasks.py
@@ -0,0 +1,17 @@
+Task1
+def foo(password):
+    if len(password) >= 8:
+        print("True")
+    else:
+        print("False")
+
+
+
+
+def foo(temperature):
+    if temperature > 7:
+        return "Warm"
+    else:
+        return "Cold"
+
+
