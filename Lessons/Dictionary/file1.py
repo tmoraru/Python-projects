ecda3d3ba7c964d84a8fbdbc243a0e0d566d0bf9
Taniusellu <tatianamoraru1998@gmail.com>
2020-07-08 04:17:22 +0000

    dictionary code

---
+++ Lessons/Dictionary/file1.py
@@ -0,0 +1,12 @@
+def mean(value):
+    if type(value) == dict:
+        the_mean = sum(value.values())  / len(value)
+    else:
+        the_mean = sum(value) / len(value)
+   
+    return the_mean
+
+monday_temperatures = [8.9, 3.5, 3.2]
+student_grades = {"Ma": 9.1, "Sim": 9.3, "GO": 9.4}
+print(mean(student_grades))
+print(mean(monday_temperatures))
