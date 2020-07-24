1f427a280cc0d8af394ff290a2e9f2cd3d4ab298
Taniusellu <tatianamoraru1998@gmail.com>
2020-07-24 07:28:00 +0000

    added

---
+++ python-school/variables/global-variables.py
@@ -0,0 +1,9 @@
+"""
+Global Variables
+Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+
+Global variables can be used by everyone, both inside of functions and outside.
+
+"""
+
+
