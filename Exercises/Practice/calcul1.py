f3d1bd85cbf750578d4532638f7f1896a51ed81d
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-08-08 21:35:27 -0500

    Update calcul1.py

--- Exercises/Practice/calcul1.py
+++ Exercises/Practice/calcul1.py
@@ -3,7 +3,7 @@
 
 #print min_value and max_value from the list
 student_grades = [8.2, 8.8, 8.9]
-max_value = max(student_grades)
+max_value = max(student_grades) 
 min_value = min(student_grades)
 print(max_value, min_value)
 
