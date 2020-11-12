c0e69f747f9132ae8025db93e26ac80b09247900
Taniusellu <tatianamoraru1998@gmail.com>
2020-11-12 17:49:22 +0000

    added

---
+++ course/1loop.py
@@ -0,0 +1,5 @@
+# infinit loop
+m = 0 
+while m <= 4:
+    print("hello")
+    m = m +1 
---
+++ course/2-infinit-loop.py
@@ -0,0 +1,8 @@
+count = 10 
+while count <= 10:
+    if count % 2 == 0:
+        count += 1 
+    print(f"we are counting: {count}")
+    count += 1 
+
+        
