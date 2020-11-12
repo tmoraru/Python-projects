9f4ee2a52b8f4b6852e82a0f113863b1919a7b35
Taniusellu <tatianamoraru1998@gmail.com>
2020-11-12 16:16:04 +0000

    added

---
+++ course/2-example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+u = "tatiana"
+password = "redhat123"
+
+username = input("Write your username:")
+password = input("Write your password:")
+
+if username == u and password == password:
+    print("success")
+else: 
+    print("false")
